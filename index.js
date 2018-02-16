8caa2ed416cedb711679563e1eb1126ca623fb28
erocks2010 <tarun.mathur@hotmail.com>
2018-02-17 01:55:10 +0530

    Table creation succesfull. Unique Curreny info being added to the table.Next requiremnt to update the table when new value flows in

--- index.js
+++ index.js
@@ -14,10 +14,13 @@ require('./site/style.css')
 // here to load the myEs6code.js file, and it will be automatically transpiled.
 
 // Change this to get detailed logging from the stomp library
-global.DEBUG = true
+global.DEBUG = false
 
-const url = "ws://localhost:8011/stomp"
+const url = "ws://localhost:8011/stomp";
+const headers = { priority: 9, id: 'sub - 0', destination: '/fx/prices' };
 const client = Stomp.client(url);
+let currenciesInOrder = [];
+let counter = 1;
 /**
  * client.hearbeat.incoming and outgoing.I think this has to do with timings mentioned in question
  */
@@ -28,15 +31,24 @@ client.debug = function (msg) {
 }
 
 function connectCallback() {
-  var sub=client.subscribe('/fx/prices', function (data) {
-  console.log('Data recieved', data);
-}, {
-    priority: 9,
-    id: 'sub - 0',
-    destination: '/fx/prices'
-  });
   console.log('Step 1 :: Connection established with Stomp Server');
-  document.getElementById('stomp-status').innerHTML = "It has now successfully connected to a stomp server serving price updates for some foreign exchange currency pairs."
+  var sub = client.subscribe('/fx/prices', function (data) {
+    let currencyInfo = JSON.parse(data.body);
+    console.log(currenciesInOrder);
+    if (currenciesInOrder.indexOf(currencyInfo.name) == -1) {
+      currenciesInOrder.push(currencyInfo.name);
+      insertRowInTable(currenciesInOrder.length, currencyInfo);
+    }
+    // console.log(Date.now())
+    // if (refreshAfter30Seconds()) {
+    //   console.log('pass data to view')
+    // }
+    // else {
+    //   console.log('do not pass data to view');
+    // }
+    console.log(refreshAfter30Seconds())
+
+  }, headers);
 }
 
 client.connect({}, connectCallback, function (error) {
@@ -44,12 +56,31 @@ client.connect({}, connectCallback, function (error) {
   alert(error.headers.message)
 })
 
+function refreshAfter30Seconds() {
+  if (counter == 10) {
+    console.log('To')
+    counter = 1;
+    return counter;
+  }
+  else {
+    counter++;
+    return counter;
+  }
+}
 
-// client.ws.onmessage=function(data){
-//   console.log(data);
-// }
-// client.on('onmessage',function(data){
-//   console.log('Data recieved',data);
-// })
+function insertRowInTable(rowIndex, currencyInfo) {
+  let table = document.getElementById('marketprice-container');
+  let row = table.insertRow(rowIndex);
+  for (let i = 0; i < 7; i++) {
+    let cell = row.insertCell(i);
+    cell.innerHTML = currencyInfo[Object.keys(currencyInfo)[i]];
+  }
+}
+
+function updateRowInTable(){
+  
+}
+
+
 const exampleSparkline = document.getElementById('example-sparkline')
 Sparkline.draw(exampleSparkline, [1, 2, 3, 6, 8, 20, 2, 2, 4, 2, 3])
