e165212eacb8e10bd9687a72d5fd17b6c3a28274
erocks2010 <tarun.mathur@hotmail.com>
2018-02-17 16:08:16 +0530

    Final Tested code.

--- index.js
+++ index.js
@@ -21,9 +21,7 @@ const headers = { priority: 9, id: 'sub - 0', destination: '/fx/prices' };
 const client = Stomp.client(url);
 let currenciesInOrder = [];
 let counter = 1;
-/**
- * client.hearbeat.incoming and outgoing.I think this has to do with timings mentioned in question
- */
+
 client.debug = function (msg) {
   if (global.DEBUG) {
     console.info(msg)
@@ -31,10 +29,8 @@ client.debug = function (msg) {
 }
 
 function connectCallback() {
-  console.log('Step 1 :: Connection established with Stomp Server');
   var sub = client.subscribe('/fx/prices', function (data) {
     let currentCurrenyInfo = JSON.parse(data.body);
-    console.log(currentCurrenyInfo);
     if (currenciesInOrder.map((currencyInfo) => currencyInfo[0]).indexOf(currentCurrenyInfo.name) == -1) {
       let _currentCurrencyInfo = objectToArray(currentCurrenyInfo);
       currenciesInOrder.push(_currentCurrencyInfo);
@@ -45,52 +41,12 @@ function connectCallback() {
       sortTable(currentCurrenyInfo);
       drawTable();
     }
-    // updateRowInTable(currenciesInOrder.indexOf(currentCurrenyInfo.name) + 1, currentCurrenyInfo)
-    // console.log(Date.now())
-    // if (refreshAfter30Seconds()) {
-    //   console.log('pass data to view')
-    // }
-    // else {
-    //   console.log('do not pass data to view');
-    // }
-    console.log(refreshAfter30Seconds())
-
   }, headers);
 }
 
 client.connect({}, connectCallback, function (error) {
-  console.log('Error encountered .Something went wrong with Stomp connection');
   alert(error.headers.message)
 })
-
-function refreshAfter30Seconds() {
-  if (counter == 10) {
-    console.log('To')
-    counter = 1;
-    return counter;
-  }
-  else {
-    counter++;
-    return counter;
-  }
-}
-
-function insertRowInTable(rowIndex, currentCurrenyInfo) {
-  let table = document.getElementById('marketprice-container');
-  let row = table.insertRow(rowIndex);
-  for (let i = 0; i < 7; i++) {
-    let cell = row.insertCell(i);
-    cell.innerHTML = currentCurrenyInfo[Object.keys(currentCurrenyInfo)[i]];
-  }
-}
-
-function updateRowInTable(rowIndex, currentCurrenyInfo) {
-  let table = document.getElementById('marketprice-container');
-  let row = table.getElementsByTagName("tr")[rowIndex];
-  for (let i = 0; i < 7; i++) {
-    let cell = row.getElementsByTagName('td')[i].innerHTML = currentCurrenyInfo[Object.keys(currentCurrenyInfo)[i]];
-  }
-}
 
 function drawTable() {
   let table = document.getElementById('marketprice-container');
@@ -101,9 +57,20 @@ function drawTable() {
       let cell = row.getElementsByTagName("td")[j];
       if (cell == undefined) cell = row.insertCell(j);
       if (j == 7) {
-        cell.innerHTML='';
+        cell.innerHTML = '';
         let sparkElement = document.createElement('span');
-        let sparkline = new Sparkline(sparkElement);
+        let sparkline = new Sparkline(sparkElement,
+          {
+            lineColor: "#666",
+            startColor: "orange",
+            endColor: "blue",
+            maxColor: "red",
+            minColor: "green",
+            dotRadius: 3,
+            tooltip: function (value, index, array) {
+              return value + ' ' + (30 - (index + 1)) + ' seconds ago';
+            }
+          });
         sparkline.draw(currenciesInOrder[i - 1][j]);
         cell.appendChild(sparkElement);
       }
@@ -120,11 +87,9 @@ function sortTable(updateCurrency) {
     let sparklineData = currenciesInOrder[indexToUpdate][7];
     let _updateCurreny = objectToArray(updateCurrency);
     _updateCurreny.push(sparklineData);
-    currenciesInOrder.splice(indexToUpdate, 1);
-    currenciesInOrder.push(_updateCurreny);
+    currenciesInOrder.splice(indexToUpdate, 1, _updateCurreny);
   }
   currenciesInOrder
-    // .map((currenyInfo)=>currencyInfo.lastChangeBid)
     .sort(function (a, b) {
       if (a[6] < b[6]) return -1;
       else return 1;
@@ -149,7 +114,3 @@ function objectToArray(object) {
   }
   return arr;
 }
-
-
-const exampleSparkline = document.getElementById('example-sparkline')
-Sparkline.draw(exampleSparkline, [1, 2, 3, 6, 8, 20, 2, 2, 4, 2, 3])
